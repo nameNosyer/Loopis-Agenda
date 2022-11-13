a8813ef0c756905421cc35a8705e0823842c3836
Reyson Renan <reysonferreira2018@gmail.com>
2022-11-13 12:14:42 -0300

    mostrando conteudo na tela

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,5 +18,39 @@ form.addEventListener("submit", (e) => {
     } else {
       console.log("success");
       msg.innerHTML = "";
+      acceptData();
+      add.setAttribute("data-bs-dismiss", "modal");
+      add.click();
+      (() => {
+        add.setAttribute("data-bs-dismiss", "");
+      })();
     }
   };
+
+ let data = {};
+
+let acceptData = () => {
+    data["text"]= textInput.value;
+    data["date"]= dateInput.value;
+    data["description"] = textarea.value;
+
+    createTasks();
+};
+
+let createTasks = () => {
+  tasks.innerHTML += `
+    <div>
+          <span class="fw-bold">${data.text}</span>
+          <span class="small text-secondary">${data.date}</span>
+          <p>${data.description}</p>
+  
+        </div>
+    `;
+    resetForm();
+};
+
+let resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+};
